4cccc89835f2d53f4944160a1fcee1b9d0f8f5e9
jessie <jessielin0422@gmail.com>
2026-05-18 11:22:52 +0800

    完成

---
+++ 01-pdo.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>PDO連線</title>
+</head>
+
+<body>
+     <h3>PDO連線</h3>
+     <?php
+     $dsn="mysql:host=localhost;charset=utf8;dbname=school";
+     $pod=new PDO($dsn,'root','');
+
+     $sql="SELECT * FROM `dept` ";
+
+     $depts=$pdo->query($sql)->fetchA11(PDO::FETCH_ASSOC);
+
+     echo "<pre>";
+     print_r($depts);
+     echo "</pre>";
+
+     $sql_insert="INSERT INTO `dept` (`code`,`name`) VALUES('601','中餐科')";
+     //$pdo->exec($sql_insert);
+     echo "<h3>新增資料</h3>";
+     echo $sql_insert;
+     echo "<hr>";
+     $dept=$pdo->query($sql)->fetchA11(PDO::FETCH_ASSOC);
+     echo "<pre>";
+     print_($depts);
+     echo "</pre>";
+
+     echo "<h3>更新資料</h3>";
+     $sql_update="UPDATE `dept` SET `code`='602' , `name`='西餐科' WHERE `id`='8';
+$pdo->exec($sql_update);
+echo $sql_update;
+echo "<hr>";
+$dept=$pdo->query($sql)->fetchA11(PDO::FETCH_ASSOC);
+echo "<pre>";
+print_r($depts);
+echo "</pre>";
+
+     echo "<h3>刪除資料</h3>";
+     $sql_delete="DELETE FROM `dept` WHERE `id`='9'";
+     $pdo->exec($sql_delete);
+     echo $sql_delete;
+     echo "<hr>";
+     $dept=$pdo->query($sql)->fetchA11(PDO::FETCH_ASSOC);
+     echo "<pre>";
+print_r($depts);
+echo "</pre>";
+
+
+     ?>
+
+     <P>&nbsp;</P>
+     <P>&nbsp;</P>
+     <P>&nbsp;</P>
+     <P>&nbsp;</P>
+     <P>&nbsp;</P>
+     <P>&nbsp;</P>
+
+
+
+</body>
+
+</html>
---
+++ 02-register.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>會員註冊</title>
+   
+
+</head>
+
+<body>
+     <div>
+          <h3>簡易註冊系統</h3>
+
+          <ul>
+               <li>建立一個資料表來存放使用者的帳號、密碼及個人資料</li>
+               <li>建立一個網頁表單可以讓使用者輸入自己的帳號、密碼及個人資料</li>
+               <li>送出表單後可以將使用者的資料存入資料表</li>
+          </ul>
+
+          <h3>資料表設計-members</h3>
+          <ul>
+               <li>ida</li>
+               <li>account</li>
+               <li>password</li>
+               <li>tel</li>
+               <li>email</li>
+               <li>birthday</li>
+          </ul>
+
+          <h3>註冊表單設計</h3>
+
+          <ul>
+               <li>經典風格(紅磚色紅帶點復古經典和墨綠色的配色點最色霧金色)</li>
+               <li>整體字體要舒服簡單</li>
+               <li>表單設計要圓潤修飾有線條</li>
+          </ul>
+     </div>
+
+
+
+</body>
+
+</html>
